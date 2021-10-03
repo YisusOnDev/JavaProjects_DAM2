14cc2bdb8cd805838e4104440cdcbf97be441294
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-10-03 13:32:03 +0200

    KrustyKrabThreads: project finished

--- KrustyKrabThreads/src/mainapp/MainApp.java
+++ KrustyKrabThreads/src/mainapp/MainApp.java
@@ -6,29 +6,35 @@ import threads.Chef;
 
 public class MainApp {
     public static void main(String[] args) throws Exception {
-        final int CHEF_COUNT = 10;
-        final int CLIENT_COUNT = 20;
-        
+        // CHEF and CLIENTS Threads count.
+        final int CHEF_COUNT = 100;
+        final int CLIENT_COUNT = 100;
+
+        // Generate an unique tray where burgers are taken from and added by the chefs.
         Tray KrustyKrabTray = new Tray();
-        
+
+        // Generate an array of chefs and clients threads.
         Chef[] KrustyChefs = new Chef[CHEF_COUNT];
         BurgerEater[] KrustyClients = new BurgerEater[CLIENT_COUNT];
-        
+
+        // Adding chef threads to the array
         for (int i = 0; i < KrustyChefs.length; i++) {
             KrustyChefs[i] = new Chef(KrustyKrabTray, i);
         }
 
+        // Adding client threads to the array
         for (int i = 0; i < KrustyClients.length; i++) {
             KrustyClients[i] = new BurgerEater(KrustyKrabTray, i);
         }
 
-        for (int i = 0; i < KrustyChefs.length; i++) {
-            KrustyChefs[i].start();
+        // Initialize chef and client threads.
+        for (Chef chef : KrustyChefs) {
+            chef.start();
         }
-        
-        for (int i = 0; i < KrustyClients.length; i++) {
-            KrustyClients[i].start();
+
+        for (BurgerEater burgerEater : KrustyClients) {
+            burgerEater.start();
         }
-            
+
     }
 }
--- KrustyKrabThreads/src/threads/Chef.java
+++ KrustyKrabThreads/src/threads/Chef.java
@@ -4,22 +4,35 @@ import java.util.Random;
 
 import criticsection.Tray;
 
+/**
+ * Chef Class
+ */
 public class Chef extends Thread {
     private boolean _RunThread;
     private Tray burgerTray;
     private int chefId;
 
+    /**
+     * Constructor for our famous Chefs
+     * 
+     * @param burgerTray Current tray that is being used
+     * @param chefId     Thread id of the chef (for identification only)
+     */
     public Chef(Tray burgerTray, int chefId) {
         this.burgerTray = burgerTray;
         this.chefId = chefId;
-        this._RunThread = true;
+        this._RunThread = true; // Flag to start a while loop when start() is called
     }
 
     @Override
     public void run() {
+        // Infinite loop
         while (_RunThread) {
+            // Simulate cook time
             int timeToCook = new Random().nextInt(3000 - 1000) + 1000;
+            // int timeToCook = new Random().nextInt(2000 - 1) + 1;
 
+            // We try to wait to take our time to cook burger and add it to the tray
             try {
                 Thread.sleep(timeToCook);
             } catch (InterruptedException ex) {
