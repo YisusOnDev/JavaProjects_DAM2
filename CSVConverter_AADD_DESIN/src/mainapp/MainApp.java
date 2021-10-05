63d7b1410ab6816c7616eb2b5b58ca6379d9116e
Jesús Téllez <jesustellez.contacto@gmail.com>
2021-10-05 20:41:05 +0200

    (CSVConverter): MainView structure semi-done

--- CSVConverter_AADD_DESIN/src/mainapp/MainApp.java
+++ CSVConverter_AADD_DESIN/src/mainapp/MainApp.java
@@ -5,11 +5,13 @@ import views.MainView;
 
 public class MainApp {
 	public static void main(String[] args) {
+		/**
 		ReadWrite.convertCSVToBinary("origCsv.csv", "origCsv.dat");
 		ReadWrite.convertBinaryToCsv("origCsv.dat", "origCsv_converted.csv");
 		ReadWrite.sortCsv("origCsv.csv", "origCsv_ord.csv");
 		ReadWrite.sortBinary("origCsv.dat", "origCsv_ord.dat");
 		ReadWrite.binaryFileToSortedCsv("origCsv.dat", "origCsv_convertedandsorted.csv");
+		**/
 		new MainView();
 	}
 
--- CSVConverter_AADD_DESIN/src/views/MainView.java
+++ CSVConverter_AADD_DESIN/src/views/MainView.java
@@ -1,30 +1,22 @@
 package views;
 
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+
 
 public class MainView {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainView window = new MainView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JLabel lblWindowTitle;
+	private JFrame frmCsvconverter;
+	private JTextPane textPane;
+	private JLabel lblPreview;
+	private JButton btnLoadCsv;
 
 	/**
 	 * Create the application.
@@ -37,12 +29,54 @@ public class MainView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel lblNewLabel = new JLabel("Pene");
-		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		frmCsvconverter = new JFrame("CSV Converter");
+		frmCsvconverter.setTitle("CSV Converter");
+		frmCsvconverter.setBounds(100, 100, 650, 450);
+		frmCsvconverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCsvconverter.getContentPane().setLayout(null);
+
+		setUIComponents();
+		setUIListeners();
+
+		frmCsvconverter.setVisible(true);
 	}
 
+	/**
+	 * Sets UI components
+	 */
+	private void setUIComponents() {
+		lblWindowTitle = new JLabel("CSV Converter");
+		lblWindowTitle.setForeground(new Color(204, 0, 0));
+		lblWindowTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWindowTitle.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
+		lblWindowTitle.setBounds(10, 11, 240, 60);
+		frmCsvconverter.getContentPane().add(lblWindowTitle);
+		
+		lblPreview = new JLabel("CSV/Data Preview");
+		lblPreview.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPreview.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblPreview.setBounds(315, 42, 309, 29);
+		frmCsvconverter.getContentPane().add(lblPreview);
+		
+		btnLoadCsv = new JButton("Load CSV");
+		btnLoadCsv.setBounds(10, 100, 89, 23);
+		frmCsvconverter.getContentPane().add(btnLoadCsv);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(315, 82, 309, 300);
+		frmCsvconverter.getContentPane().add(scrollPane);
+		
+		textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+		textPane.setBackground(Color.LIGHT_GRAY);
+
+		frmCsvconverter.setResizable(false);
+		frmCsvconverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/**
+	 * Sets UI Elements Listeners...
+	 */
+	private void setUIListeners() {
+	}
 }
