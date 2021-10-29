8cbd8c1edb9f68599aed0489fb47ded9e67ea377
Jesús T <52821987+YisusOnDev@users.noreply.github.com>
2021-10-29 11:48:49 +0200

    Added AddressApp (built w/ JavaFX)

---
+++ AddressApp_FX_AADD/src/mainapp/RunApp.java
@@ -0,0 +1,13 @@
+package mainapp;
+
+import javafx.application.Application;
+
+public class RunApp {
+	/**
+	 * Class to avoid Maven JavaFX problems
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Application.launch(MainApp.class);
+	}
+}
